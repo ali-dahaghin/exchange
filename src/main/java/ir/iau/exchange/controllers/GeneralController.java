12475d45e37ea1fcfbaca64e4e4cf1677da3208d
A10 <alidahaghin1@gmail.com>
2023-06-05 13:30:03 +0330

    make give a visible for admin in navbar

--- src/main/java/ir/iau/exchange/controllers/GeneralController.java
+++ src/main/java/ir/iau/exchange/controllers/GeneralController.java
@@ -2,11 +2,14 @@ package ir.iau.exchange.controllers;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class GeneralController {
@@ -19,7 +22,9 @@ public class GeneralController {
         if (authentication instanceof AnonymousAuthenticationToken) {
             modelAndView.addObject("auth", false);
         } else {
+            Optional<? extends GrantedAuthority> roleAdmin = authentication.getAuthorities().stream().filter(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN")).findAny();
             modelAndView.addObject("auth", true);
+            modelAndView.addObject("isAdmin", roleAdmin.isPresent());
         }
 
         return modelAndView;
