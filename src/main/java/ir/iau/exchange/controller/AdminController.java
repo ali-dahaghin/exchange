5aeb46fb9928531b9566f41002e3da646628e12f
A10 <alidahaghin1@gmail.com>
2023-06-11 11:50:03 +0330

    add users page for admin

--- src/main/java/ir/iau/exchange/controller/AdminController.java
+++ src/main/java/ir/iau/exchange/controller/AdminController.java
@@ -1,6 +1,8 @@
 package ir.iau.exchange.controller;
 
+import ir.iau.exchange.dto.UserDto;
 import ir.iau.exchange.dto.requestes.GiveADto;
+import ir.iau.exchange.entity.User;
 import ir.iau.exchange.exception.BadRequestRuntimeException;
 import ir.iau.exchange.service.AdminService;
 import ir.iau.exchange.service.UserService;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
@@ -41,6 +46,20 @@ public class AdminController {
         } catch (BadRequestRuntimeException badRequestRuntimeException) {
             return new ModelAndView("redirect:/admin/give?userNotExists");
         }
+    }
+
+    @GetMapping("/users")
+    public ModelAndView users() {
+        ModelAndView modelAndView = new ModelAndView("/admin/users");
+        List<User> dbUsers = userService.findAll();
+        List<UserDto> userDtoList = dbUsers.stream().map(dbUser -> {
+            UserDto userDto = new UserDto();
+            userDto.setUsername(dbUser.getUsername());
+            userDto.setIsAdmin(dbUser.getRoles().stream().anyMatch(role -> role.getName().equals("ROLE_ADMIN")));
+            return userDto;
+        }).collect(Collectors.toList());
+        modelAndView.addObject("users", userDtoList);
+        return modelAndView;
     }
 
 }
---
+++ src/main/java/ir/iau/exchange/dto/UserDto.java
@@ -0,0 +1,11 @@
+package ir.iau.exchange.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String username;
+    private Boolean isAdmin;
+
+}
