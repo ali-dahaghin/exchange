bfaedaf7e7c782b882477ab88ace56a3743fb1c3
0xA10 <alidahaghin1@gmail.com>
2023-08-20 14:39:30 +0330

    move payment history page and moved it to dashboard

--- src/main/java/ir/iau/exchange/controller/UserController.java
+++ src/main/java/ir/iau/exchange/controller/UserController.java
@@ -1,6 +1,9 @@
 package ir.iau.exchange.controller;
 
+import ir.iau.exchange.dto.PaymentHistoryDto;
+import ir.iau.exchange.entity.User;
 import ir.iau.exchange.entity.UserAsset;
+import ir.iau.exchange.service.PaymentService;
 import ir.iau.exchange.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,12 +18,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PaymentService paymentService;
+
     @GetMapping("/user/dashboard")
     public ModelAndView home() {
         List<UserAsset> currentUserAssets = userService.getCurrentUserAssets();
 
-        ModelAndView modelAndView = new ModelAndView("/user/dashboard");
+        User currentUser = userService.getCurrentUser();
+        List<PaymentHistoryDto> historyForUser = paymentService.getHistoryForUser(currentUser.getUsername());
+
+        ModelAndView modelAndView = new ModelAndView("user/dashboard");
+        modelAndView.addObject("historyForUser", historyForUser);
         modelAndView.addObject("userAssets", currentUserAssets);
+        modelAndView.addObject("username", currentUser.getUsername());
+
         return modelAndView;
     }
 
